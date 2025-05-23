bfb805330fa43986215f5fe85c3f0563fe61fe77
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2025-05-22 19:51:46 -0600

    fix: remove repeated folders

--- src/types/index.jsx
+++
@@ -1,8 +0,0 @@
-import ForgotPasswordFormPropTypes from "./Login-Register/Forgot-password/Forgot-password-form/ForgotPasswordFormPropTypes"
-import LoginFormPropTypes from "./Login-Register/Login/LoginForm/LoginFormPropTypes/LoginFormPropTypes"
-import RegisterFormPropTypes from './Login-Register/Register/RegisterForm/RegisterFormPropTypes/RegisterFormPropTypes'
-export{
-    LoginFormPropTypes,
-    RegisterFormPropTypes,
-    ForgotPasswordFormPropTypes
-}
